91f9006b09df79fac884c8ee1f4b08473fedf6cd
Alesya Khoroneko <horoneko00@gmail.com>
2020-04-19 19:04:13 +0300

    Add user editing feature

---
+++ src/main/java/by/training/khoroneko/command/redirect/UserEditCommand.java
@@ -0,0 +1,28 @@
+package by.training.khoroneko.command.redirect;
+
+import by.training.khoroneko.builder.UserBuilder;
+import by.training.khoroneko.command.Command;
+import by.training.khoroneko.command.JSPParameter;
+import by.training.khoroneko.command.Pages;
+import by.training.khoroneko.exception.ServiceException;
+import by.training.khoroneko.service.impl.UserServiceImpl;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class UserEditCommand implements Command {
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            request.setAttribute(JSPParameter.USER_PROFILE.getValue(), new UserServiceImpl().findById(
+                    new UserBuilder()
+                            .setId(Integer.parseInt(request.getParameter(JSPParameter.USER_ID.getValue())))
+                            .getResult()));
+            return Pages.USER_EDIT_JSP.getValue();
+        } catch (ServiceException e) {
+            request.setAttribute(JSPParameter.ERROR_MASSAGE.getValue(), e.getMessage());
+            return Pages.ERROR_JSP.getValue();
+        }
+
+    }
+}
