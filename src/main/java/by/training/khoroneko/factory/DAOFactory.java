5930a79543a4ad92a100250259abe03038c1b1cc
Alesya Khoroneko <horoneko00@gmail.com>
2020-04-20 14:58:14 +0300

    Add DrinkDAO to DAOFactory

--- src/main/java/by/training/khoroneko/factory/DAOFactory.java
+++ src/main/java/by/training/khoroneko/factory/DAOFactory.java
@@ -1,14 +1,19 @@
 package by.training.khoroneko.factory;
 
+import by.training.khoroneko.dao.impl.DrinkDAOImpl;
 import by.training.khoroneko.dao.impl.UserDAOImpl;
 
 public enum DAOFactory {
     INSTANCE;
 
     private UserDAOImpl userDAO = new UserDAOImpl();
+    private DrinkDAOImpl drinkDAO = new DrinkDAOImpl();
 
     public UserDAOImpl getUserDAO() {
         return userDAO;
     }
 
+    public DrinkDAOImpl getDrinkDAO() {
+        return drinkDAO;
+    }
 }
