0286d03cbf8e8596eadbc3f44918654b076fb091
Alesya Khoroneko <horoneko00@gmail.com>
2020-04-27 11:52:05 +0300

    Add user profile

--- src/main/java/by/training/khoroneko/command/Pages.java
+++ src/main/java/by/training/khoroneko/command/Pages.java
@@ -10,7 +10,8 @@ public enum Pages {
     DRINK_LIST_JSP("page/drinks.jsp"),
     DRINK_MANAGEMENT_JSP("page/drinkmanagement.jsp"),
     DRINK_ADDING_JSP("page/add_drink.jsp"),
-    DRINK_ADDING_SERVINGS_JSP("page/add_servings.jsp");
+    DRINK_ADDING_SERVINGS_JSP("page/add_servings.jsp"),
+    USER_PROFILE_JSP("page/user_profile.jsp");
 
     private String value;
 
