4b3b3555b622d2686ccfd10d0f662d4f336d7760
Alesya Khoroneko <horoneko00@gmail.com>
2020-05-02 23:14:58 +0300

    Add attaching card to user

---
+++ src/main/java/by/training/khoroneko/dao/UserDAO.java
@@ -0,0 +1,16 @@
+package by.training.khoroneko.dao;
+
+import by.training.khoroneko.entity.User;
+import by.training.khoroneko.exception.DAOException;
+
+public interface UserDAO {
+    User findByEmailAndPassword(User user) throws DAOException;
+
+    User findByEmail(User user) throws DAOException;
+
+    User findById(User user) throws DAOException;
+
+    void updateUserInfoById(User user) throws DAOException;
+
+    void attachCardToUserById(User user) throws DAOException;
+}
