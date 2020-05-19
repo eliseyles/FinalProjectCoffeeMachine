0871e0120a6de5907be1d0639d807f6a22e5d3d2
Alesya Khoroneko <horoneko00@gmail.com>
2020-05-19 20:41:24 +0300

    Add f5 protection

--- src/main/java/by/training/khoroneko/command/Pages.java
+++ src/main/java/by/training/khoroneko/command/Pages.java
@@ -1,32 +1,47 @@
 package by.training.khoroneko.command;
 
 public enum Pages {
-    INDEX_JSP("page/index.jsp"),
-    REGISTRATION_JSR("page/registration.jsp"),
-    ERROR_500_JSP("page/error500.jsp"),
-    SIGN_IN_JSP("page/sign_in.jsp"),
-    DRINK_LIST_JSP("page/drinks.jsp"),
+    INDEX_JSP("page/index.jsp", "INDEX_PAGE"),
+    REGISTRATION_JSR("page/registration.jsp","REGISTRATION_PAGE"),
+    ERROR_500_JSP("page/error500.jsp", "500"),
+    SIGN_IN_JSP("page/sign_in.jsp", "SIGN_IN_PAGE"),
+    DRINK_LIST_JSP("page/drinks.jsp", "DRINK_LIST"),
 
-    USER_LIST_JSP("page/admin/user_list.jsp"),
-    USER_EDIT_JSP("page/admin/user_edit.jsp"),
-    DRINK_MANAGEMENT_JSP("page/admin/drink_management.jsp"),
-    DRINK_ADDING_JSP("page/admin/add_drink.jsp"),
-    DRINK_ADDING_SERVINGS_JSP("page/admin/add_servings.jsp"),
+    USER_LIST_JSP("page/admin/user_list.jsp", "USER_LIST"),
+    USER_EDIT_JSP("page/admin/user_edit.jsp", "USER_EDIT"),
+    DRINK_MANAGEMENT_JSP("page/admin/drink_management.jsp", "DRINK_MANAGEMENT"),
+    DRINK_ADDING_JSP("page/admin/add_drink.jsp", "ADD_DRINK_PAGE"),
+    DRINK_ADDING_SERVINGS_JSP("page/admin/add_servings.jsp", "ADD_SERVINGS_PAGE"),
 
-    USER_PROFILE_JSP("page/user/user_profile.jsp"),
-    EDIT_PROFILE_JSP("page/user/edit_profile.jsp"),
-    ADD_CARD_JSP("page/user/add_card.jsp"),
-    EDIT_CARD_JSP("page/user/edit_card.jsp"),
-    ADD_MONEY_JSP("page/user/add_money.jsp"),
-    CART_JSP("page/user/cart.jsp");
+    USER_PROFILE_JSP("page/user/user_profile.jsp", "USER_PROFILE"),
+    EDIT_PROFILE_JSP("page/user/edit_profile.jsp", "EDIT_PROFILE_PAGE"),
+    ADD_CARD_JSP("page/user/add_card.jsp", "ADD_CARD_PAGE"),
+    EDIT_CARD_JSP("page/user/edit_card.jsp", "EDIT_CARD_PAGE"),
+    ADD_MONEY_JSP("page/user/add_money.jsp", "ADD_MONEY_PAGE"),
+    CART_JSP("page/user/cart.jsp", "CART_PAGE");
 
     private String value;
+    private String commandName;
 
-    Pages(String value) {
+    Pages(String value, String commandName) {
         this.value = value;
+        this.commandName = commandName;
     }
 
     public String getValue() {
         return value;
+    }
+
+    public String getCommandName() {
+        return commandName;
+    }
+
+    public static String getCommandNameByPath(String path) {
+        for (Pages page : Pages.values()) {
+            if (page.getValue().equals(path)) {
+                return page.getCommandName();
+            }
+        }
+        return ERROR_500_JSP.getCommandName();
     }
 }
